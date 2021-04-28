32bfb88efe95baaefa15247daf3418d64df796cf
j <jsn.chn@outlook.com>
2021-04-28 16:17:17 -0700

    Add computerPlay() function
    Updated readme, added script tag to html, added computerPlay()
    function that randomly returns "rock", "paper", or "scissors".
    Test runs computerPlay() 10x.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rockPaperScissors
 Odin Project 2
+
+https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,13 @@
+function computerPlay() {
+    let rng = Math.floor(Math.random() * 3)
+    if (rng < 1)
+        return "Rock";
+    else if (rng < 2)
+        return "Paper";
+    else
+        return "Scissors";
+}
 
+for (i = 0; i < 10; i++) {
+    console.log(computerPlay())
+}
