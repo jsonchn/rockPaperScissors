e67074fbc740fbdcafbc65d888e62abdba233787
j <jsn.chn@outlook.com>
2021-04-28 19:53:09 -0700

    Finish Project 2 features Added global variables to track wins, playRound() function to play a round of RPS, and game() function to prompt player input and play RPS 5x and declare the winner of 5 rounds.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,73 @@
+// declare globals
+let round = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function computerPlay() {
+    // randomly returns "Rock", "Paper", or "Scissors"
     let rng = Math.floor(Math.random() * 3)
     if (rng < 1)
-        return "Rock";
+        return "ROCK";
     else if (rng < 2)
-        return "Paper";
+        return "PAPER";
     else
-        return "Scissors";
+        return "SCISSORS";
 }
 
-for (i = 0; i < 10; i++) {
-    console.log(computerPlay())
+function playRound(playerSelection, computerSelection){
+    // dictionary to store win conditions
+    let winCon = {"ROCK": "SCISSORS", "SCISSORS": "PAPER", "PAPER": "ROCK"};
+
+    // initialize roundResult
+    let roundResult = "";
+    
+    // capitalize playerSelection argument
+    let capPlayerSelection = playerSelection.toUpperCase();
+
+    // tie condition
+    if (capPlayerSelection == computerSelection) {
+        roundResult = "It's a tie! " + capPlayerSelection + " ties with " + 
+            computerSelection + "!";
+    }
+
+    // win condition
+    else if (winCon[capPlayerSelection] == computerSelection) {
+        roundResult = "You win! " + capPlayerSelection + " beats " + 
+            computerSelection + "!";
+        playerWins++;
+    }
+
+    // lose condition
+    else {
+        roundResult = "You lose! " + computerSelection + " beats " +
+            capPlayerSelection + "!";
+        computerWins++;
+    }
+    round++;
+    return roundResult;
+}
+
+function game(){
+    let playerInput;
+    for (i = 0; i < 5; i++) {
+        playerInput = prompt("ROCK, PAPER, or SCISSORS?");
+        console.log(playRound(playerInput, computerPlay()))
+        round++;
+    }
+}
+
+while (round < 5) {
+    game();
+}
+if (round >= 5) {
+    if (playerWins > computerWins) {
+        console.log("Player wins with " + playerWins + " wins!");
+    }
+    else if (computerWins > playerWins) {
+        console.log("Computer wins with " + computerWins + " wins!")
+    }
+    else {
+        console.log("It's a tie! End score was " + playerWins + " - " + 
+            playerWins);
+    }
 }
